0c8b42261ee5e85effde486158f8740ffcb2f55c
Dylan Duault <dylan.duault@gmail.com>
2025-02-09 16:41:54 +0100

    feat: wip homepage

--- resources/js/Pages/Homepage.vue
+++ resources/js/Pages/Homepage.vue
@@ -25,8 +25,8 @@ function handleImageError() {
             <el-main>
                 <el-row class="mb-6">
                     <el-col :span="24">
-                        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900">Discover Egypt Like Never Before!</h1>
-                        <p class="mb-6 text-lg font-normal text-gray-500 lg:text-xl">Embark on a journey through time and explore the wonders of Ancient Egypt. <br> Visit the Pyramids of Giza, sail along the Nile, admire breathtaking temples, and immerse yourself in the rich history of the Pharaohs.</p>
+                        <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-yellow-900">Discover Egypt Like Never Before!</h1>
+                        <p class="mb-6 text-lg font-normal text-yellow-700 lg:text-xl">Embark on a journey through time and explore the wonders of Ancient Egypt. <br> Visit the Pyramids of Giza, sail along the Nile, admire breathtaking temples, and immerse yourself in the rich history of the Pharaohs.</p>
                     </el-col>
                 </el-row>
                 <el-row class="mb-6">
@@ -36,18 +36,47 @@ function handleImageError() {
                 </el-row>
             </el-main>
         </el-container>
-        <el-container class="container-padding bg-yellow-whiter">
+        <el-container class="container-padding bg-yellow-darker">
             <el-main>
-                <el-row class="w-100 mb-6" :gutter="20">
-                    <el-col :span="12">
-                        <el-card>
-                            FILL CONTENT
-                        </el-card>
-                    </el-col>
-                    <el-col :span="12">
-                        <el-card>
-                            FILL CONTENT
-                        </el-card>
+                <el-row class="mb-6">
+                    <el-col :span="24" class="text-center">
+                        <h2 class="text-3xl font-bold text-yellow-900 mb-20">Why Choose us?</h2>
+                        <el-row :gutter="20" class="mb-5">
+                            <el-col :span="12">
+                                <el-card shadow="hover" class="h-full yellow-card">
+                                    <div class="text-center">
+                                        <h3 class="text-xl font-semibold mb-2">Expert Guides</h3>
+                                        <p>Passionate Egyptologists for an authentic experience</p>
+                                    </div>
+                                </el-card>
+                            </el-col>
+                            <el-col :span="12">
+                                <el-card shadow="hover" class="h-full yellow-card">
+                                    <div class="text-center">
+                                        <h3 class="text-xl font-semibold mb-2">Tailor-Made Trips</h3>
+                                        <p>Customizable tours to suit your preferences</p>
+                                    </div>
+                                </el-card>
+                            </el-col>
+                        </el-row>
+                        <el-row :gutter="20">
+                            <el-col :span="12">
+                                <el-card shadow="hover" class="h-full yellow-card">
+                                    <div class="text-center">
+                                        <h3 class="text-xl font-semibold mb-2">Premium Accommodations</h3>
+                                        <p>Stay in top-rated hotels and luxury boats</p>
+                                    </div>
+                                </el-card>
+                            </el-col>
+                            <el-col :span="12">
+                                <el-card shadow="hover" class="h-full yellow-card">
+                                    <div class="text-center">
+                                        <h3 class="text-xl font-semibold mb-2">Unique Experiences</h3>
+                                        <p>Exclusive access to historical sites</p>
+                                    </div>
+                                </el-card>
+                            </el-col>
+                        </el-row>
                     </el-col>
                 </el-row>
             </el-main>
@@ -66,16 +95,33 @@ function handleImageError() {
 
 <style scoped>
 
-.bg-yellow {
+.bg-yellow-darker {
     background-color: #ffe0af;
 }
 
-.bg-yellow-whiter {
+.bg-yellow {
     background-color: #ffeed4;
 }
 
+.yellow-card {
+    background-color: #ffeed4;
+    border-color: #ffe0af;
+
+    h3 {
+        color: rgb(113 63 18 / var(--tw-text-opacity, 1))
+    }
+
+    p {
+        color: rgb(161 98 7 / var(--tw-text-opacity, 1))
+    }
+}
+
 .container-padding {
-    padding: 64px 10rem 64px 10rem;
+    padding: 64px 1rem 64px 1rem;
+
+    @media screen and (min-width: 992px) {
+        padding: 64px 10rem 64px 10rem;
+    }
 }
 
 </style>
