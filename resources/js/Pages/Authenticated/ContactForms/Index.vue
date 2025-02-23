b6a97d1e9c6e177e38a7ea2051188e0b7c3387bb
Dylan Duault <dylan.duault@gmail.com>
2025-02-23 21:21:06 +0100

    build: fix

--- resources/js/Pages/Authenticated/ContactForms/Index.vue
+++ resources/js/Pages/Authenticated/ContactForms/Index.vue
@@ -3,6 +3,7 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
 
 interface IContactForm {
+    id: number;
     fullname: string;
     email: string;
     trip_dates: string;
